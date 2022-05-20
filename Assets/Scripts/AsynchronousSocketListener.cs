add80b4539c752e94e1501a92b11abbc7f0645c5
Katarn <fabien@themonkeyparadox.com>
2022-05-20 19:04:30 +0200

    Set server to Localhost

--- Assets/Scripts/AsynchronousSocketListener.cs
+++ Assets/Scripts/AsynchronousSocketListener.cs
@@ -34,7 +34,7 @@ public class AsynchronousSocketListener
         // Establish the local endpoint for the socket.  
         // The DNS name of the computer  
         // running the listener is "host.contoso.com".  
-        IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+        IPHostEntry ipHostInfo = Dns.GetHostEntry("localhost");
         IPAddress ipAddress = ipHostInfo.AddressList[0];
         IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
 
